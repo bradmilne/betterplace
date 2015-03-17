0989f91265d4c4fd4d8735c2d74c9d45c0403a3f
Brad Milne <brad.milne@getelevate.com>
2015-03-17 10:53:28 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Betterplace::Application.config.secret_key_base = '4e14c11d5bfda19907257e1531c4ca1de44db3dc6b3fa213710ae8edf9b817236503ce8d168ca26cdc11f086ba5cf84eda8376ff5909071eced799fdf49afaa8'
