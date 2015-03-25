802738a11fe293229cbe80afc1af8927240519bd
Brad Milne <brad.milne@getelevate.com>
2015-03-25 11:08:51 -0400

    change to environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,10 @@ Betterplace::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w( bootstrap.css )
+
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
